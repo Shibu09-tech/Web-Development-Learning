8a17f60ae84c9be855ff6c6fec0d7f54b9419820
Shibu09-tech <raishibu09@gmail.com>
2026-01-25 13:43:17 +0530

    Error related to file handling handled

--- NodeJS/Server(Nginx)/server.js
+++ NodeJS/Server(Nginx)/server.js
@@ -5,7 +5,8 @@ const path = require("path")
 const port =3000
 
 const server = http.createServer((req,res) =>{
-    const filepath = path.join(__dirname, req.url ==='/' ? "index1.html" : "req.url")
+    const filepath = path.join(__dirname, req.url ==='/' ? "index1.html" : req.url);
+    console.log(filepath);
 
     const extName = String(path.extname(filepath)).toLowerCase()
 
